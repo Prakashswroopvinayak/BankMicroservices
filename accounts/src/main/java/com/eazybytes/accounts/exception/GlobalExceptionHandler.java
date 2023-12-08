a1c845ec0591d917dc6a00672e8bc8bcbc521ad2
Prakash Swaroop Vinayak <tiwariprakash965@gmail.com>
2023-12-08 19:13:09 +0530

    Runtime exception handled and Data Validation

--- accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -2,22 +2,54 @@ package com.eazybytes.accounts.exception;
 
 
 import com.eazybytes.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> validationError = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+        validationErrorList.forEach( (error) ->{
+                String fieldName = ((FieldError)error).getField();
+                String validatingMsg = error.getDefaultMessage();
+                validationError.put(fieldName,validatingMsg);
+                }
+        );
+        return new ResponseEntity<>(validationError, HttpStatus.BAD_REQUEST);
+
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                                 WebRequest webRequest){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     // need to understand couple of thing
     @ExceptionHandler(CustomerAlreadyExistsException.class)
-
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
                                                                                  WebRequest webRequest){
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
@@ -30,7 +62,6 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
                                                                                  WebRequest webRequest){
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
